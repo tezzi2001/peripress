a575b7386bf1294f6e8925cd7885d982aa210906
Bohdan Bondarenko <bohdan.bondarenko2001@gmail.com>
2020-09-25 17:10:15 +0300

    add annotations for wiring controllers

---
+++ src/main/java/org/bondarenko/core/annotation/Mapping.java
@@ -0,0 +1,13 @@
+package org.bondarenko.core.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(value= RetentionPolicy.RUNTIME)
+public @interface Mapping {
+    String method();
+    String url();
+}
