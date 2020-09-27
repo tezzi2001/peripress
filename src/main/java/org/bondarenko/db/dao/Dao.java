4c3348ddf871db75546799a63363acec65e8b711
Bohdan Bondarenko <bohdan.bondarenko2001@gmail.com>
2020-09-27 23:44:08 +0300

    add generic DAO interface

---
+++ src/main/java/org/bondarenko/db/dao/Dao.java
@@ -0,0 +1,12 @@
+package org.bondarenko.db.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Dao<T> {
+    public Optional<T> find(long id);
+    public List<T> findAll();
+    public boolean save(T entity);
+    public boolean delete(long id);
+    public boolean delete(T entity);
+}
